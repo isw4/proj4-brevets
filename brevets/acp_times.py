2e5b9b2dd6aa353b43fba2c2906248833cacc497
isw4 <isw4@pitt.edu>
2017-10-16 17:48:19 -0700

    build up acp_times functions and associated automated tests

--- brevets/acp_times.py
+++ brevets/acp_times.py
@@ -4,7 +4,7 @@ for ACP-sanctioned brevets
 following rules described at https://rusa.org/octime_alg.html
 and https://rusa.org/pages/rulesForRiders
 """
-import arrow
+import arrow, math
 
 #  Note for CIS 322 Fall 2016:
 #  You MUST provide the following two functions
@@ -14,6 +14,10 @@ import arrow
 #  same arguments.  Arguments are explained in the
 #  javadoc comments.
 #
+
+CUTOFF =  [0, 200, 400, 600, 1000]   #cutoff distance brackets for each speed bracket (km)
+MAXSPD =  [0, 34 , 32 , 30 , 28]     #maximum speed for each time bracket (km/h)
+MINSPD =  [0, 15 , 15 , 15 , 11.428] #minimum speed for eachg time bracket (km/h)
 
 
 def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -29,7 +33,19 @@ def open_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control open time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    assert brevet_dist_km >= 0 and brevet_dist_km <= 1000
+    if control_dist_km > brevet_dist_km: raise ValueError("Provided control distance is greater than provided Brevet distance")
+    if control_dist_km < 0:              raise ValueError("Provided control distance is less than 0")
+
+    if control_dist_km == 0: time = 0
+    else:                    time = find_time_using(MAXSPD, control_dist_km)
+    
+    start = arrow.get(brevet_start_time)
+    hrs, mins = convert_hrs_mins(time)
+    shifted = start.shift(hours=+hrs)
+    shifted = shifted.shift(minutes=+mins)
+    #print("the open time is: "+str(hrs)+":"+str(mins))
+    return shifted.isoformat()
 
 
 def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
@@ -45,4 +61,52 @@ def close_time(control_dist_km, brevet_dist_km, brevet_start_time):
        An ISO 8601 format date string indicating the control close time.
        This will be in the same time zone as the brevet start time.
     """
-    return arrow.now().isoformat()
+    assert brevet_dist_km >= 0 and brevet_dist_km <= 1000
+    if control_dist_km > brevet_dist_km: raise ValueError("Provided control distance is greater than provided Brevet distance")
+    if control_dist_km < 0:              raise ValueError("Provided control distance is less than 0")
+
+    if control_dist_km == 0: time = 0
+    else:                    time = find_time_using(MINSPD, control_dist_km)
+    
+    start = arrow.get(brevet_start_time)
+    hrs, mins = convert_hrs_mins(time)
+    shifted = start.shift(hours=+hrs)
+    shifted = shifted.shift(minutes=+mins)
+    #print("the open time is: "+str(hrs)+":"+str(mins))
+    return shifted
+
+def find_time_using(spd, control_dist_km):
+    """
+    Calculates the time to travel the control distance given by the speeds in the spd variable. It iterates
+    through the distance cutoff array, then increments the time appropriately if control > CUTOFF[i]. Once the 
+    control distance is within its appropriate bracket, the time is incremented from the remaining distance.
+    Args:
+    	spd: list of numbers, of appropriate speeds (should either be the constant arrays MAXSPD or MINSPD)
+    	control_dist_km: number, the control distance in kilometers
+    Returns:
+    	The time it takes to travel the control distance, calculated from the speed brackets given
+    """
+    time = 0
+    rem = control_dist_km
+    for i in range(1, len(CUTOFF)):
+        if control_dist_km > CUTOFF[i]:
+            delta = CUTOFF[i] - CUTOFF[i-1];
+            time += delta/spd[i]
+            rem -= delta
+        else:
+            time += rem/spd[i]
+            break
+    return time
+
+def convert_hrs_mins(f):
+    """
+    Converts a time expressed as a decimal into time expressed as hours and minutes.
+    Args:
+    	Time expressed in a floating point number
+    Returns: 
+    	Time expressed in an list of [hours, minutes]
+    """
+    mins, hrs = math.modf(f)
+    hrs = int(hrs)
+    mins = int(mins*60)
+    return [hrs, mins]
---
+++ brevets/tests/test_acp_times.py
@@ -0,0 +1,103 @@
+from acp_times import convert_hrs_mins, find_time_using, open_time, close_time
+import arrow
+
+# Testing convert_hrs_mins
+def test_convert_00_00():
+	time = 0
+	hrs, mins = convert_hrs_mins(time)
+	assert int(hrs) == 0
+	assert int(mins) == 0
+
+def test_convert_05_20():
+	time = 5.45
+	hrs, mins = convert_hrs_mins(time)
+	assert int(hrs) == 5
+	assert int(mins) == 27
+
+
+# Testing find_time_using(MAXSPD, distance)
+def test_find_time_max_100():
+	time = find_time_using(acp_times.MAXSPD, 100)
+	assert time - 2.93 < 0.02	#margin: +-1.2 minutes
+
+def test_find_time_max_300():
+	time = find_time_using(acp_times.MAXSPD, 300)
+	assert time - 9.00 < 0.02
+
+def test_find_time_max_500():
+	time = find_time_using(acp_times.MAXSPD, 500)
+	assert time - 15.47 < 0.02
+
+def test_find_time_max_900():
+	time = find_time_using(acp_times.MAXSPD, 900)
+	assert time - 29.52 < 0.02
+
+
+# Testing find_time_using(MINSPD, distance)
+def test_find_time_min_100():
+	time = find_time_using(acp_times.MINSPD, 100)
+	assert time - 6.67 < 0.02	#margin: +-1.2 minutes
+
+def test_find_time_min_300():
+	time = find_time_using(acp_times.MINSPD, 300)
+	assert time - 20.00 < 0.02
+
+def test_find_time_min_500():
+	time = find_time_using(acp_times.MINSPD, 500)
+	assert time - 33.33 < 0.02
+
+def test_find_time_min_900():
+	time = find_time_using(acp_times.MINSPD, 900)
+	assert time - 66.25 < 0.02
+
+
+# Testing open_time
+def test_open_neg():
+	try:
+		open_time(-1, 600, arrow.utcnow().isoformat())
+		assert False
+	except ValueError:
+		pass
+
+def test_open_1001():
+	try:
+		open_time(1001, 600, arrow.utcnow().isoformat())
+		assert False
+	except ValueError:
+		pass
+
+def test_open_0():
+	here = arrow.utcnow().isoformat()
+	assert here == open_time(0, 600, here)
+
+def test_open_1000():
+	here = arrow.utcnow().isoformat()
+	there = here.shift(hours+=33)
+	there = there.shift(minutes+=5)
+	there == open_time(0, 600, here)
+
+
+# Testing close_time
+def test_close_neg():
+	try:
+		close_time(-1, 600, arrow.utcnow().isoformat())
+		assert False
+	except ValueError:
+		pass
+
+def test_close_1001():
+	try:
+		close_time(1001, 600, arrow.utcnow().isoformat())
+		assert False
+	except ValueError:
+		pass
+
+def test_close_0():
+	here = arrow.utcnow().isoformat()
+	assert here == close_time(0, 600, here)
+
+def test_close_1000():
+	here = arrow.utcnow().isoformat()
+	there = here.shift(hours+=51)
+	there = there.shift(minutes+=0)
+	there == close_time(0, 600, here)
